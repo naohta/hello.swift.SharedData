76663e59fa78fe902fcd8291604ed04c5877503e
Naohiro OHTA <naohta@gmail.com>
2015-06-22 14:14:40 +0900

    It works!

---
+++ README.md
@@ -0,0 +1,10 @@
+Shared data saving place in Swift
+=================================
+
+
+Based on the below Japanese web page about objc, I think about that Shared data saving place in Swift.
+
+Objcについて書かれた下記ウェブページを基にして、Swift用に改めて、Sharedなdataの保存場所を考えた。
+
+> 入門書を書いている連中は最初にこれぐらいのことをガツンを書けよハゲ！
+> http://d.hatena.ne.jp/omaemona01/20111204/1322961924
--- hello.swift.SharedData/ViewController.swift
+++ hello.swift.SharedData/ViewController.swift
@@ -1,25 +1,40 @@
-//
-//  ViewController.swift
-//  hello.swift.SharedData
-//
-//  Created by Naohiro OHTA on Jun22,2015.
-//  Copyright © 2015 Naohiro OHTA. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+
+        // MARK: - [A] Instance variables
+
+        let delegate1 = UIApplication.sharedApplication().delegate
+        print("------------------------------------------")
+        print("UIApplication.sharedApplication().delegate")
+        print("------------------------------------------")
+        dump(delegate1)
+        print("")
+
+
+        // MARK: - [B] Static variables  *RECOMMENDED by Me*
+
+        let var1 = AppDelegate.static_var_on_appDelegate_1
+        print("------------------------------------------")
+        print("AppDelegate.static_var_on_appDelegate_1")
+        print("------------------------------------------")
+        print(var1.dynamicType)
+        dump(var1)
+        print("")
+
+
+        let var2 = AppDelegate.static_var_on_appDelegate_2
+        print("------------------------------------------")
+        print("AppDelegate.static_var_on_appDelegate_2")
+        print("------------------------------------------")
+        print(var2.dynamicType)
+        dump(var2)
+        print("")
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
