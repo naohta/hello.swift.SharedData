a7cc27152430f687de2072e0294ccea16a24a7d1
Naohiro OHTA <naohta@gmail.com>
2015-06-22 14:10:11 +0900

    1st

---
+++ hello.swift.SharedData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello.swift.SharedData
+//
+//  Created by Naohiro OHTA on Jun22,2015.
+//  Copyright © 2015 Naohiro OHTA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
